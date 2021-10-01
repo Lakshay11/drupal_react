0592170a2a56bdf1d8359f8ef8db41be5fd74102
lakshay1602 <lakshay.kumar@srijan.net>
2021-10-01 14:16:19 +0530

    DR-00: inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+export default class App extends React.Component {
+  // define constructor
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      node: [],
+      loading: true,
+      error: null,
+    };
+  }
+
+  // Life cycle 
+  componentDidMount() {
+    axios
+      .get(
+        window.encodeURI(
+          `https://drupal9.info/api/article/4`,
+        ),
+      )
+      .then(response => {
+        const node = response.data;
+        this.setState({
+          node,
+          loading: false,
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error: error,
+          loading: false,
+        });
+      });
+  }
+
+  // Loading state 
+  renderLoading() {
+    return (
+      <div>
+        Loading...
+      </div>
+    );
+  }
+
+  // error state 
+  renderError() {
+    return (
+      <div>
+        <div>
+          Sorry, an error ocurred: {this.state.error.response.data.message}
+        </div>
+      </div>
+    );
+  }
+
+  // output state 
+  renderList() {
+    const { error } = this.state;
+
+    if (error) {
+      console.log(error);
+      return this.renderError();
+    }
+
+    return (
+      <div className="Content-wrapper">
+          {
+            this.state.node.map(d=>
+              <div className="content-container">{d.title}</div>)
+          }
+      </div>
+    );
+  }
+
+  render() {
+    return this.state.loading ? this.renderLoading() : this.renderList();
+  }
 }
-
-export default App;
